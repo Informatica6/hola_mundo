3cbf9b25efcc0b92d2f12187980544a3eb0312a6
informatica6 <36902883+Informatica6@users.noreply.github.com>
2018-03-21 13:53:54 +0100

    bjcalsedc

--- main.f95
+++ main.f95
@@ -2,7 +2,7 @@ program main
 
 use algebra_lineal
 
-real::Xi,Xd,I !para la integral por trapecios n se define como real y para la de simpson como entero
+real::Xi,Xd,I!,n !para la integral por trapecios n se define como real y para la de simpson y Riemann como entero
 integer :: n
 
 write(*,*)'elige un intervalo de integracion'
@@ -11,14 +11,14 @@ read(*,*)Xi !valor inicial de x
 read(*,*)Xd !valor final de x
 
 write(*,*)'el intervalo es:',Xi,Xd
-write(*,*)'elige un numero de subintervalos' !trapecio !riemann
-!write(*,*)'elige el numero de repeticiones de la integral' !simpson
+!write(*,*)'elige un numero de subintervalos' !trapecio !riemann
+write(*,*)'elige el numero de repeticiones de la integral' !simpson
 
 read(*,*)n
 
 !call trapecio(f1,Xi,Xd,n,I)
-!call simpson(Xi,Xd,n,I,f1)
-call Riemann(Xi,Xd,n,I,f1)
+call simpson(Xi,Xd,n,I,f1)
+!call Riemann(Xi,Xd,n,I,f1)
 
 write(*,*)'el area de la integral es:',I
 
