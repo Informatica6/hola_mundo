8f558398eb9f6b1fde39a686fe1017b4a90458ab
Pablo <pablols100@gmail.com>
2018-05-08 15:41:39 +0200

    juego de los zombie

---
+++ Juego.f95
@@ -0,0 +1,18 @@
+Program Juego
+
+    use funciones
+    use algrebra_lineal
+
+    implicit none
+
+    Real(8)                 ::
+    Integer                 :: 
+    Real(8), allocatable    ::
+
+
+
+
+
+
+    !call system(python G_Juego.py)
+end program Juego
